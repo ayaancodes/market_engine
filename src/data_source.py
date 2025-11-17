9e4e8b7df386c3e795be630148150cd8477ae72f
Ayaan Munshi <ayaanmunshi456@gmail.com>
2025-11-16 20:57:05 -0500

    Implemented data cleaning w/Pandas

--- src/data_source.py
+++ src/data_source.py
@@ -2,15 +2,16 @@ import kagglehub
 import os
 import shutil
 
-def download_SP500_Data():
+def download_XAUGold_Data():
     #Returns the path of where the dataset was downloaded
-    path = kagglehub.dataset_download("wmcginn/sp500-csv")
+    path = kagglehub.dataset_download("novandraanugrah/xauusd-gold-price-historical-data-2004-2024")
     print("Path to dataset files:", path)
     return path
 
-folder_path = download_SP500_Data()
-price_file = folder_path+"/prices.csv"
-destination_file = "/Users/ayaanmunshi/Desktop/Coding/Projects/Market_Engine/data/processed/sp500_prices.csv"
+folder_path = download_XAUGold_Data()
+print("Files in the folder:", os.listdir(folder_path))
+single_day_pricing = folder_path+"/XAU_1d_data.csv"
+destination_file = "/Users/ayaanmunshi/Desktop/Coding/Projects/Market_Engine/data/processed/xau_daily.csv"
 
 
-shutil.copyfile(price_file, destination_file)
+shutil.copyfile(single_day_pricing, destination_file)
